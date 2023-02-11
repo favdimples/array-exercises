9d15f73ee843a47ab9124d18196646f44f4c52d9
favdimples <rahel.elena.ackeret@gmail.com>
2023-02-11 14:43:32 +0100

    idk

--- script.js
+++ script.js
@@ -4,24 +4,27 @@ document.getElementById('original').innerHTML = beatles
 // Exercise 1
 
 
-
+document.getElementById("array-ex1").innerHTML = beatles[1]+ " " + beatles[3]
 
 
 // Exercise 2
-
+beatles[4]="rachel"
+document.getElementById("array-ex2").innerHTML = beatles
 
 
 // Exercise 3 
 
-
+beatles[4]="5"
+document.getElementById("array-ex3").innerHTML = beatles
 
 // Exercise 4 
 
- beatles
+ beatles[10]="5"
+ document.getElementById("array-ex4").innerHTML = beatles
 
 // Exercise 5
 
-
+`beatles $[beatles.lenght] elements`
 
 // Exercise 6
 
